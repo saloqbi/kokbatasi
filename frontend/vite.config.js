535f32355280691404b113d42d4c47fef980de13
saloqbi <saloqbi@gmail.com>
2025-06-10 23:15:34 +0300

    ✅ fix: move index.html to root and adjust vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'public',         // اجعل الجذر مجلد public
-  publicDir: '../public', // تشير إلى نفس المجلد للثوابت الأخرى
   plugins: [react()],
-  build: {
-    outDir: '../dist',    // أو أي مجلد تريد البناء فيه
-  }
 })
