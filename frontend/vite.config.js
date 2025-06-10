001eff065d30fe57045d28250e370cc25f5f34dd
saloqbi <saloqbi@gmail.com>
2025-06-10 22:50:56 +0300

    ✅ F

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // إن لزم
-  publicDir: '../public', // فقط إذا كنت تستخدم root: 'src'
 })
