9820f6880932d60236ccc7c4aa25c12d95428799
saloqbi <saloqbi@gmail.com>
2025-06-10 21:46:35 +0300

    update Main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ManualSignal from './pages/ManualSignal';
-import AllSignals from './pages/AllSignals';
-import SignalDetails from './pages/SignalDetails';
-import Navigation from './components/Navigation';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/manual-signal" element={<ManualSignal />} />
-        <Route path="/signals" element={<AllSignals />} />
-        <Route path="/signals/:id" element={<SignalDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {
+  return <h1>✅ التطبيق يعمل الآن!</h1>;
 }
+
+export default App;
