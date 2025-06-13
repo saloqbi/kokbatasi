b820ed654964064044ebc8a9514ffb79bcefe245
saloqbi <saloqbi@gmail.com>
2025-06-13 09:35:07 +0300

    ✅ add signal model

--- backend/models/Signal.js
+++ backend/models/Signal.js
@@ -1,13 +1,17 @@
-
 const mongoose = require('mongoose');
 
-const signalSchema = new mongoose.Schema({
-  symbol: String,
-  action: String,
-  price: Number,
-  type: String,
-  date: String,
-  data: Array
-}, { timestamps: true });
+const signalSchema = new mongoose.Schema(
+  {
+    symbol: String,
+    price: Number,
+    type: String,
+    date: String,
+    data: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Signal', signalSchema);
