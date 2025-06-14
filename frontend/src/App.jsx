55814e1e0cc73a9a683977f12bf9e7ba04d5f179
saloqbi <saloqbi@gmail.com>
2025-06-15 01:45:01 +0300

    ✅ نقل ملفات الواجهة النهائية إلى src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllSignalsPage from './pages/AllSignalsPage';
-import AdminDashboard from './pages/AdminDashboard';
-import LoginPage from './pages/LoginPage';
-import SignalDetailsPage from './pages/SignalDetailsPage';
-import ManualSignalPage from './pages/ManualSignalPage';
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllSignalsPage from "./pages/AllSignalsPage";
+import SignalDetailsPage from "./pages/SignalDetailsPage";
+import ManualSignalPage from "./pages/ManualSignalPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AllSignalsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/signals/:id" element={<SignalDetailsPage />} />
         <Route path="/manual-signal" element={<ManualSignalPage />} />
       </Routes>
